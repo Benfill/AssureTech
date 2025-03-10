9b4f1dbbc3688dae908ff5a5cc44f80ed57850e1
Mohammed El Bachiri <mohammed-el-bachiri@users.noreply.github.com>
2025-03-10 09:59:03 +0000

    policy routed

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -5,6 +5,8 @@ import { AuthProvider } from "./context/authContext";
 import Clients from "./components/clients/Clients";
 import AuthPage from "./pages/auth/AuthPage";
 import ProtectedRoute from "./utils/ProtectedRoute";
+import Contracts from "./pages/policy/contract/Contracts";
+import Claims from "./pages/policy/claim/Claims";
 
 const App: React.FC = () => {
   return (
@@ -16,11 +18,14 @@ const App: React.FC = () => {
             {/* Protected Route */}
             <Route element={<ProtectedRoute />}>
               <Route path="/" element={<Clients />} />
-
             </Route>
 
             {/* Public Route */}
             <Route path="/auth" element={<AuthPage />} />
+
+            {/* Policy Route */}
+            <Route path="/policy/claims" element={<Claims />} />
+            <Route path="/policy/contracts" element={<Contracts />} />
 
             {/* Fallback Route */}
             <Route path="/*" element={<h1>Not Found</h1>} />
